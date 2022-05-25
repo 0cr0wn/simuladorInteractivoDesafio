4ae70ce52b5201198ecf03ae621fcf9e5ba67dcd
Ocrown <tuliocrown@gmail.com>
2022-05-24 22:29:57 -0400

    Simulador de compra

--- dist/main.dev.js
+++ dist/main.dev.js
@@ -1,56 +1,63 @@
 "use strict";
 
 //Simulador interactivo
-var carrito = function carrito(sku1, sku2, sku3, iva) {
-  var total = sku1 + sku2 + sku3 + sku1 + sku2 + sku3 * iva;
-  return total;
+var total;
+var compraFinal; // Funcion Carrito
+
+var compra = function compra(sku) {
+  total = (sku.valor + sku.valor + sku.valor) * 1.19;
+  compraFinal = total;
+  return compraFinal;
 };
 
-alert("Hola! bienvenido a ???? compra 3 productos con los siguientes codigos `123456789´ `987654321´ y/o `135798642´");
+var nombreCliente = prompt("ingrese nombre del cliente");
+alert("Hola! bienvenido " + nombreCliente + " compra 3 productos con los siguientes codigos `123456789´ `987654321´ y/o `135798642´");
 var productos = Number(prompt("ingrese la cantidad de productos (max.3)"));
 
-while (productos > 3) {
+while (productos <= 1 && productos > 3) {
   alert("Debe llevar maximo 3 productos");
   productos = Number(prompt("ingrese la cantidad de productos (max.3)"));
 }
 
-for (var i = 0; i > productos; i++) {
-  var nombreCliente = prompt("ingrese nombre del cliente");
-  var sku1 = {
-    nombre: "producto1",
-    codigo: 123456789,
-    valor: 45000
-  };
-  var sku2 = {
-    nombre: "producto2",
-    codigo: 987654321,
-    valor: 48000
-  };
-  var sku3 = {
-    nombre: "producto3",
-    codigo: 135798642,
-    valor: 42000
-  };
-  var sku = Number(prompt("Ingrese el codigo del producto"));
+var sku1 = {
+  nombre: "producto1",
+  codigo: 123456789,
+  valor: 45000
+};
+var sku2 = {
+  nombre: "producto2",
+  codigo: 987654321,
+  valor: 48000
+};
+var sku3 = {
+  nombre: "producto3",
+  codigo: 135798642,
+  valor: 42000
+};
+var sku = [sku1, sku2, sku3];
+
+for (var i = 0; i < productos; i++) {
+  sku = Number(prompt("Ingrese el codigo del producto"));
 
   while (sku != sku1.codigo || sku != sku2.codigo || sku != sku3.codigo) {
-    sku = Number(prompt("Ingrese el codigo del producto"));
-
     if (sku == sku1.codigo || sku == sku2.codigo || sku == sku3.codigo) {
       alert("Producto agregado satisfactoriamente");
     } else {
       alert("El codigo es incorrecto, ingrese un codigo de nuestros productos.");
+      sku = Number(prompt("Ingrese el codigo del producto"));
     }
+
+    break;
   }
 
-  var total = carrito(sku1, sku2, sku3, 0.19);
-  alert("el total de la compra más iva es " + total);
   var cliente = {
     nombre: nombreCliente,
     sku1: sku1,
     sku2: sku2,
     sku3: sku3,
-    carrito: total
+    carrito: compraFinal
   };
   console.log(cliente);
 }
+
+alert("el total de la compra más iva es " + compraFinal); // Funcion Carrito
--- main.js
+++ main.js
@@ -1,64 +1,92 @@
 //Simulador interactivo
-const carrito = (sku1, sku2, sku3, iva) => {
-    let total = sku1 + sku2 + sku3 + sku1 + sku2 + sku3 * iva;
-    return total;
+let total
+
+
+
+
+let compraFinal
+// Funcion Carrito
+const compra = (sku) => {
+    total =
+        (sku.valor + sku.valor + sku.valor) * 1.19;
+    compraFinal = total;
+    return compraFinal;
+
+
 };
 
+const nombreCliente = prompt("ingrese nombre del cliente");
 alert(
-    "Hola! bienvenido a ???? compra 3 productos con los siguientes codigos `123456789´ `987654321´ y/o `135798642´"
+    "Hola! bienvenido " + nombreCliente + " compra 3 productos con los siguientes codigos `123456789´ `987654321´ y/o `135798642´"
 );
+
 
 let productos = Number(prompt("ingrese la cantidad de productos (max.3)"));
 
-while (productos > 3) {
+while (productos <= 1 && productos > 3) {
     alert("Debe llevar maximo 3 productos");
     productos = Number(prompt("ingrese la cantidad de productos (max.3)"));
 }
 
-for (let i = 0; i > productos; i++) {
-    const nombreCliente = prompt("ingrese nombre del cliente");
 
-    let sku1 = {
-        nombre: "producto1",
-        codigo: 123456789,
-        valor: 45000,
-    };
+let sku1 = {
+    nombre: "producto1",
+    codigo: 123456789,
+    valor: 45000,
+};
 
-    let sku2 = {
-        nombre: "producto2",
-        codigo: 987654321,
-        valor: 48000,
-    };
+let sku2 = {
+    nombre: "producto2",
+    codigo: 987654321,
+    valor: 48000,
+};
 
-    let sku3 = {
-        nombre: "producto3",
-        codigo: 135798642,
-        valor: 42000,
-    };
+let sku3 = {
+    nombre: "producto3",
+    codigo: 135798642,
+    valor: 42000,
+};
 
-    let sku = Number(prompt("Ingrese el codigo del producto"));
+let sku = [sku1, sku2, sku3]
+
+for (let i = 0; i < productos; i++) {
+
+    sku = Number(prompt("Ingrese el codigo del producto"));
 
     while (sku != sku1.codigo || sku != sku2.codigo || sku != sku3.codigo) {
-        sku = Number(prompt("Ingrese el codigo del producto"));
+
+
         if (sku == sku1.codigo || sku == sku2.codigo || sku == sku3.codigo) {
             alert("Producto agregado satisfactoriamente");
+
         } else {
             alert(
-                "El codigo es incorrecto, ingrese un codigo de nuestros productos."
+                "El codigo es incorrecto, ingrese un codigo de nuestros productos.",
+
+
             );
+            sku = Number(prompt("Ingrese el codigo del producto"));
+
         }
+        break
+
+
+
     }
 
-    let total = carrito(sku1, sku2, sku3, 0.19);
-    alert("el total de la compra más iva es " + total);
 
     let cliente = {
         nombre: nombreCliente,
         sku1: sku1,
         sku2: sku2,
         sku3: sku3,
-        carrito: total,
+        carrito: compraFinal,
     };
 
     console.log(cliente);
+
 }
+
+
+alert("el total de la compra más iva es " + compraFinal);
+// Funcion Carrito
